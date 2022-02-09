d500565a101831e70b966b396717ad28ba648f3a
BlueHouseSkip <33537595+BlueHouseSkip@users.noreply.github.com>
2022-02-08 18:37:48 -0800

    lab 4

---
+++ lab4/js/lab.js
@@ -0,0 +1,15 @@
+// Create a fly that buzzes aroud the page and can be swatted.
+// A fly image will fly around the screen irraticly and randomly and when clicked, it will act as if swatted and fall down.
+// @link idk what this is for.
+// @file no file.
+// @author John Brandes.
+// @since  2022.8.2.
+
+//Instantiate the fly
+  //make the fly an object related to its image
+//pick a starting position
+  //pick a place off screen for the fly to start and fly in from
+//move the fly
+  //pick a random position and turn the fly to face it and have it move there
+//check for click event
+  //run a script that checks for a mouse click and check if its in the right position
