2114340726959d763b1fdf2750a1cffc601c78f5
BlueHouseSkip <33537595+BlueHouseSkip@users.noreply.github.com>
2022-02-20 14:05:10 -0800

    lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,20 @@
+/**
+ * Author:    John Brandes
+ * Created:   2022.2.11
+ *
+ * CC0 public domain
+ **/
+
+var make = "Volkswagen";
+var model = "Jetta";
+var year = 2014;
+var color = "navy blue";
+var currentYear = 2022
+var ownIt = false;
+var age = currentYear - year;
+
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
