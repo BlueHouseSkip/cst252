38f6eb7830a733f94f26c39fd7bb62660b6bbb59
BlueHouseSkip <33537595+BlueHouseSkip@users.noreply.github.com>
2022-02-27 20:32:00 -0800

    lab 8 modified

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -5,13 +5,14 @@
  * CC0
  **/
 
- var myTransport = ["Jetta", "feet", "bike", "bus", "light-rail"];
- var myMainRide = {
-   make:"Volkswagen",
-   model:"Jetta",
-   color:"navy blue",
-   year:2014
- };
- console.log("test");
- document.writeln("My Main Ride: <pre>",
-    JSON.stringify(myMainRide, null, '\t'), "</pre>");
+var a = [123,12,5,26,1,46,341,753,8,9,548,2,65,7,46,3,512,5,31,23,534,45,6,456,31,5,4,344,7,4,5,32];
+
+function mathPow2(x) {
+    return Math.pow(x,2);
+}
+
+document.write(a.map(mathPow2) + "\n\n");
+
+document.write(a.map(function(x) {
+    return Math.pow(x,5);
+}));
