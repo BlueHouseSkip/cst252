3d9d78a949a2ffcaee87610c57722e742a61754b
BlueHouseSkip <bluehouseskip@gmail.com>
2022-03-09 19:48:59 -0800

    lab 12 improved

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -6,8 +6,16 @@
  **/
 
 
+//
 for(let i=0;i<$(".rainbow-toggle").length;i++) {
   $(".rainbow-toggle").eq(i).click(function () {
       $(".rainbow-toggle").eq(i).parent().toggleClass("rainbow-text");
     });
 }
+
+//Changes the color of the buttons to match the background color of the page every 50 miliseconds.
+function updateButtonColor() {
+  let color = $("body").css("background-color");
+  $(".rainbow-toggle").css("background-color", color);
+};
+setInterval(updateButtonColor, 50);
