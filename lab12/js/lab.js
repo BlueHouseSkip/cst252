8ada3e5009ea59e6041c80807c91a35aad7ddd35
BlueHouseSkip <33537595+BlueHouseSkip@users.noreply.github.com>
2022-03-12 16:06:44 -0800

    changed lab12

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -16,6 +16,9 @@ for(let i=0;i<$(".rainbow-toggle").length;i++) {
 //Changes the color of the buttons to match the background color of the page every 50 miliseconds.
 function updateButtonColor() {
   let color = $("body").css("background-color");
+  let textColor = $("#title").css("color");
   $(".rainbow-toggle").css("background-color", color);
+  $(".rainbow-toggle").css("color", textColor);
+  return;
 };
 setInterval(updateButtonColor, 50);
