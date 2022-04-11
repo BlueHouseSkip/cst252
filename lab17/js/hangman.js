f8835d640afbef64c82608ffc5e32a4b58f663d2
BlueHouseSkip <33537595+BlueHouseSkip@users.noreply.github.com>
2022-04-10 23:25:52 -0700

    lab17

---
+++ lab17/js/hangman.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+the board:
+ - creates itself
+ - updates itself every frame
+ - looks for input from input handler
+ - makes sure the inputs are valid
+ - displays the inputs
+
+
+input handler
+ - looks for player inputs from player 1 and 2
+ - sends it to the board
+
+
+graphics
+ - displays the game
+ - creates the area the game will exist within
